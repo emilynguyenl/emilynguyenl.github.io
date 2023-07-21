a490945eac765dc0634b101adc8847946063b751
Emily Nguyen <emilynguyen@Emilys-MacBook-Pro-2.local>
2023-07-21 11:32:50 -0700

    added about section code

---
+++ test.py
@@ -0,0 +1,23 @@
+class Emily:
+    def aboutMe():
+        fullName = "Emily Nguyen"
+        homeTown = "Phoenix, Arizona"
+        university = "Chapman University"
+        major = "Computer Science"
+        minors = ["Pyschology", "Analytics"]
+        expectedGradDate = 2024
+        interestsAndHobbies = [
+            "Roller Skating",
+            "Traveling",
+            "Puzzles",
+            "Cooking",
+            "Baseball"
+        ]
+
+    def connect():
+        resume = 'pdf'
+        email = 'enguye24@gmail.com'
+        links = [
+            "LinkedIn",
+            "Github"
+        ]
